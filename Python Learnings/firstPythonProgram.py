84b6d9e0b9336128dad7539e6a196605bdfddea0
mrgaurav.jain@hotmail.com <mrgaurav.jain@hotmail.com>
2025-08-25 17:12:05 +0530

    z

--- Python Learnings/firstPythonProgram.py
+++ Python Learnings/firstPythonProgram.py
@@ -1,3 +1,11 @@
 print("hello World!")
-name=input("what is your Name?")
+name=input("what is your Name? " )
 print("Welcome to Python", name)
+#This is a comment test
+''' this is a multiline comment that i am trying to enable here in python lets c if 
+its works out or not '''
+
+print('''this is a multiline comment that i am trying to enable here in python,
+      lets c if 
+
+      its works out or not ''')
